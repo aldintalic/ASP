1a4c1499fc7811234a8a5029b06128bee6d87151
Aldin Talic <46074429+talicnitom@users.noreply.github.com>
2019-01-30 02:35:08 +0100

    Update DfsAlgoritamIterativno.h

--- Graf/DfsAlgoritamIterativno.h
+++ Graf/DfsAlgoritamIterativno.h
@@ -1,78 +1,51 @@
 #pragma once
 
-
 #include <iostream>
 
+#include "StekSekv.h"
 #include "ListaPov.h"
-#include "PrioritetniRedPov.h"
-#include "Ivica.h"
-#include"MatricaSusjedstva.h"
-#include"BfsAlgoritamIterativno.h"
+#include "ListaSekv.h"
 using namespace std;
 
-class KruskalAlgoritam
+class DfsAlgoritamIterativno
 {
 private:
-	KruskalAlgoritam(MatricaSusjedstva* M){
-		//ulazna matrica susjedstva
-		grafMatrica = M;
+	int startVrh;
+	MatricaSusjedstva* M;
 
-		//PR koji ?uva sortirane ivice koje su formirane na osnovu ulazne matrice susjedstva 
-		grafIvice = new PrioritetniRedPov<Ivica*>;
-
-		//ovo je rezultat algoritma (stablo minimalnog raspona, MST)
-		stabloIvice = new ListaPov<Ivica*>;
-
-		//pomo?na matrica susjedstva za stablo (MST) koja se koristi radi Dfs i Bfs algoritma kod provjere ciklusa	
-		stabloMatrica = new MatricaSusjedstva(M->velicina);
+	DfsAlgoritamIterativno(MatricaSusjedstva* M, int startVrh){
+		this->startVrh = startVrh;
+		this->M = M;
 	}
 
-	MatricaSusjedstva* grafMatrica;
-	PrioritetniRed<Ivica*>* grafIvice;
-	MatricaSusjedstva* stabloMatrica;
-	Lista<Ivica*>* stabloIvice;
+	Lista<int>* _start(){
+		Lista<int>* posjeceni = new ListaSekv<int>;
 
-	void pripremiIvice(){
-		for (int fromVrh = 0; fromVrh < grafMatrica->velicina; fromVrh++){
-			Lista<int>* susjedi = grafMatrica->susjediTo(fromVrh);
-			for (int toIndex = 0; toIndex < susjedi->count(); toIndex++){
-				int toVrh = susjedi->get(toIndex);
-				int tezina = grafMatrica->tezina(fromVrh, toVrh);
-				Ivica* ivica = new Ivica(fromVrh, toVrh, tezina);
-				grafIvice->add(ivica);
+		StekSekv<int> stek;
+		stek.dodaj(startVrh);
+
+		while (!stek.isPrazan()){
+
+			int v = stek.ukloni();
+
+			if (!posjeceni->sadrzi(v)) {
+
+				posjeceni->dodaj(v);
+
+				for (int i = M->velicina; i >= 0; i--) {
+					if (M->isSusjed(v, i)){
+						if (!posjeceni->sadrzi(i))
+							stek.dodaj(i);
+					}
+				}
 			}
 		}
+		return posjeceni;
 	}
 
-	bool isCiklus(Ivica* i){
-		Lista<int>* lista = BfsAlgoritamIterativno::start(stabloMatrica, i->endVrh);
-		return lista->sadrzi(i->startVrh);
-	}
-
-	Lista<Ivica*>* _start(){
-		pripremiIvice();
-
-		int brojac = 0;
-		int s = 0;
-
-		while (brojac < (stabloMatrica->velicina) - 1){
-			Ivica* i = grafIvice->ukloni();
-
-			if (!isCiklus(i)){
-				stabloIvice->dodaj(i);
-				stabloMatrica->dodaj(i);
-				s += i->tezina;
-				brojac++;
-			}
-		}
-		cout << "Kruskal Algoritam: MST suma je " << s << endl;
-
-		return stabloIvice;
-	}
 
 public:
-
-	static Lista<Ivica*>* start(MatricaSusjedstva* M){
-		return KruskalAlgoritam(M)._start();
+	static Lista<int>* start(MatricaSusjedstva* M, int startVrh = 0){
+		return DfsAlgoritamIterativno(M, startVrh)._start();
 	}
 };
